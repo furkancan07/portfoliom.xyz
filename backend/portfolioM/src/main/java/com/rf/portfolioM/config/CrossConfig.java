8079922624bbdd421e8e5339e6a42dbf37d2f5f9
furkancan07 <canfurkan903@gmail.com>
2025-02-12 21:12:12 +0300

    .

--- backend/portfolioM/src/main/java/com/rf/portfolioM/config/CrossConfig.java
+++ backend/portfolioM/src/main/java/com/rf/portfolioM/config/CrossConfig.java
@@ -1,2 +1,14 @@
-package com.rf.portfolioM.config;public class CrossConfig {
+package com.rf.portfolioM.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CrossConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
 }
